909c50b87de8da2b3495c883bd922648d5561337
Andrii Maiskyi <andrey@wearehelful.com>
2025-07-17 23:37:16 +0100

    feat: handlers. #5

--- mcpbox/src/mcp-file-handler/sagas/create-server/create-server.saga.ts
+++ mcpbox/src/mcp-file-handler/sagas/create-server/create-server.saga.ts
@@ -1,6 +1,15 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ICommand, ofType, Saga } from '@nestjs/cqrs';
-import { map, merge, Observable, tap } from 'rxjs';
+import {
+  map,
+  merge,
+  Observable,
+  tap,
+  combineLatest,
+  filter,
+  mergeMap,
+  from,
+} from 'rxjs';
 
 // Events
 import { NewServerFoundEvent } from '../../events/new-server-found';
@@ -8,6 +17,7 @@ import { CreateDraftServerSucceedEvent } from '../../events/create-draft-server-
 import { UpdateServerOverviewSucceedEvent } from '../../events/update-server-overview-succeed';
 import { UpdateServerOwnerSucceedEvent } from '../../events/update-server-owner-succeed';
 import { UpdateServerCategorySucceedEvent } from '../../events/update-server-category-succeed';
+import { GetServerReadmeSucceedEvent } from '../../events/get-server-readme-succeed';
 // Commands
 import { CreateDraftServerCommand } from '../../commands/create-draft-server';
 import { UpdateServerOverviewCommand } from '../../commands/update-server-overview';
@@ -45,9 +55,51 @@ export class CreateServerSaga {
       ),
     );
 
+    const readme$ = events$.pipe(
+      ofType(GetServerReadmeSucceedEvent),
+      mergeMap(({ event }: GetServerReadmeSucceedEvent) => {
+        return from([
+          new UpdateServerOwnerCommand(event),
+          new UpdateServerCategoryCommand(event),
+          new UpdateServerOverviewCommand(event),
+        ]);
+      }),
+      tap((command) => {
+        if (command.constructor.name === UpdateServerOwnerCommand.name) {
+          this.logger.log(
+            `Updating server owner: ${command.command.data.title}`,
+          );
+        }
+        if (command.constructor.name === UpdateServerCategoryCommand.name) {
+          this.logger.log(
+            `Updating server category: ${command.command.data.title}`,
+          );
+        }
+        if (command.constructor.name === UpdateServerOverviewCommand.name) {
+          this.logger.log(
+            `Updating server overview: ${command.command.data.title}`,
+          );
+        }
+      }),
+    );
+
+    // const combined$ = combineLatest([owner$]).pipe(
+    //   filter(([owner]) =>
+    //     [owner.command.documentId].every((id, _, arr) => id === arr[0]),
+    //   ),
+    //   map(([{}]) => {
+    //     return new PublishServerCommand({
+    //       serverId: overview.serverId,
+    //       title: overview.title,
+    //       // ...you can merge data from all three
+    //     });
+    //   }),
+    // );
+
     return merge(
       new$,
       draft$,
+      readme$,
       // events$.pipe(
       //   ofType(UpdateServerOverviewSucceedEvent),
       //   map(
