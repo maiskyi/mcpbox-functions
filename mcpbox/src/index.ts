030f380b75ba3c5b8ffb6a83b1a0add52518f9df
Andrii Maiskyi <andrey@wearehelful.com>
2025-07-22 17:40:41 +0100

    feat: deploy. #7

--- mcpbox/src/index.ts
+++ mcpbox/src/index.ts
@@ -4,13 +4,13 @@ import { defineSecret } from 'firebase-functions/params';
 import { HandlersModule } from './app.module';
 import { McpFileHandlerService } from './mcp-file-handler';
 
-export const strapiApiToken = defineSecret('STRAPI_API_TOKEN');
+const strapiApiToken = defineSecret('STRAPI_API_TOKEN');
 
-export const strapiSchemaUrl = defineSecret('STRAPI_GRAPHQL_SCHEMA_URL');
+const strapiSchemaUrl = defineSecret('STRAPI_GRAPHQL_SCHEMA_URL');
 
-export const strapiBaseUrl = defineSecret('STRAPI_API_BASE_URL');
+const strapiBaseUrl = defineSecret('STRAPI_API_BASE_URL');
 
-export const openaiApiKey = defineSecret('OPENAI_API_KEY');
+const openaiApiKey = defineSecret('OPENAI_API_KEY');
 
 export const handleOnMcpFileCreatedUpdated = onObjectFinalized(
   {
